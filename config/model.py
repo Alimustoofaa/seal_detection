42feb75d6ebcdbcdcf3bb16e6b8b3e939603d814
Alimustoofaa <softhouse.ali@gmail.com>
2022-02-28 23:43:50 +0700

    edit version mode

--- config/model.py
+++ config/model.py
@@ -10,8 +10,8 @@ DIRECTORY_LOGGER        = os.path.expanduser('~/.Halotec/Loggers')
 #============================ MODELS ======================================
 DETECTION_MODEL = {
 	'seal_detection' : {
-		'filename'  : 'seal_detection.pt',
-		'url'       : 'https://www.dropbox.com/s/wwa953l07tpflml/seal_detection.pt?dl=1',
+		'filename'  : 'seal_detection_v1_2.pt',
+		'url'       : 'https://www.dropbox.com/s/wwa953l07tpflml/seal_detection_v1_1.pt?dl=1',
 		'file_size' : 14753191
 	}
 }
